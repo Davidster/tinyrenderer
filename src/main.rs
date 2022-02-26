2c210cf5a42d3a3862f85e426f0d4340b3fca56a
David Huculak <davidhuculak5@gmail.com>
2022-02-26 14:26:43 -0500

    checkpont: done 'Textures and bind groups' section including challenge

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod helpers;
 mod mesh_component;
+mod texture;
 mod transform;
-
-use std::borrow::Cow;
 
 use helpers::*;
 use mesh_component::*;
 use transform::*;
 
+use image::GenericImageView;
 use wgpu::util::DeviceExt;
 
 const FRAME_WIDTH: i64 = 1080;
@@ -31,56 +31,68 @@ struct State {
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
-    render_pipeline: wgpu::RenderPipeline,
-    pentagon_shape_buffers: ShapeBuffers,
-    star_shape_buffers: ShapeBuffers,
-    chosen_shape: ChosenShape,
+
     current_mouse_pos: Option<winit::dpi::PhysicalPosition<f64>>,
+    chosen_shape: ChosenShape,
+
+    star_render_pipeline: wgpu::RenderPipeline,
+    star_shape_buffers: ShapeBuffers,
+    star_diffuse_texture_bind_group: wgpu::BindGroup,
+    star_diffuse_texture: texture::Texture,
+
+    pentagon_render_pipeline: wgpu::RenderPipeline,
+    pentagon_shape_buffers: ShapeBuffers,
+    pentagon_diffuse_texture_bind_group: wgpu::BindGroup,
+    pentagon_diffuse_texture: texture::Texture,
 }
 
 type VertexPosition = [f32; 3];
 type VertexColor = [f32; 3];
+type VertexTextureCoords = [f32; 2];
 
-// main.rs
 #[repr(C)]
 #[derive(Copy, Clone, Debug)]
-struct Vertex {
+struct ColoredVertex {
     position: VertexPosition,
     color: VertexColor,
 }
 
-unsafe impl bytemuck::Pod for Vertex {}
-unsafe impl bytemuck::Zeroable for Vertex {}
+unsafe impl bytemuck::Pod for ColoredVertex {}
+unsafe impl bytemuck::Zeroable for ColoredVertex {}
 
-impl Vertex {
-    const _ATTRIBS: [wgpu::VertexAttribute; 2] =
+impl ColoredVertex {
+    const ATTRIBS: [wgpu::VertexAttribute; 2] =
         wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
 
     fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
-        // verbose version:
         wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            array_stride: std::mem::size_of::<ColoredVertex>() as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[
-                wgpu::VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-                wgpu::VertexAttribute {
-                    offset: std::mem::size_of::<VertexPosition>() as wgpu::BufferAddress,
-                    shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-            ],
+            attributes: &Self::ATTRIBS,
         }
+    }
+}
 
-        // concise version:
-        // wgpu::VertexBufferLayout {
-        //     array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
-        //     step_mode: wgpu::VertexStepMode::Vertex,
-        //     attributes: &Self::_ATTRIBS,
-        // }
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+struct TexturedVertex {
+    position: VertexPosition,
+    tex_coords: VertexTextureCoords,
+}
+
+unsafe impl bytemuck::Pod for TexturedVertex {}
+unsafe impl bytemuck::Zeroable for TexturedVertex {}
+
+impl TexturedVertex {
+    const ATTRIBS: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x2];
+
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<TexturedVertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS,
+        }
     }
 }
 
@@ -101,6 +113,7 @@ impl State {
         // a^2 + b^2 = c^2 -> b = sqrt(c^2 - a^2) where a is 0.5 and c is 1
         let t_height = ((1.0 - (0.5 * 0.5)) as f32).sqrt();
         let half_t_height = t_height / 2.0;
+        let star_height = t_2_offset + t_height;
 
         // triangle pointing upwards
         let t_1 = vec![
@@ -119,31 +132,66 @@ impl State {
             .iter()
             .map(|val| [val[0], val[1] - t_2_offset])
             .collect();
-        let star_vertices: Vec<Vertex> = t_1
+        let star_vertices: Vec<TexturedVertex> = t_1
             .iter()
             .chain(t_2_translated.iter())
-            .map(|pos| Vertex {
+            .map(|pos| {
                 // move it up so it's centered at the origin
-                position: [pos[0], pos[1] + (t_2_offset / 2.0), 0.0],
-                color: star_color.clone(),
+                [pos[0], pos[1] + (t_2_offset / 2.0), 0.0]
+            })
+            .map(|position| TexturedVertex {
+                position,
+                tex_coords: [
+                    (position[0] + 0.5),
+                    (position[1] + (star_height / 2.0)) / star_height,
+                ],
+                // tex_coords: pos
+                //     .to_vec()
+                //     .iter()
+                //     .map(|val| (val + 1.0) / 2.0)
+                //     .collect::<Vec<f32>>()
+                //     .as_slice()
+                //     .try_into()
+                //     .unwrap(),
             })
             .collect();
 
+        dbg!(&star_vertices);
+
         let star_indices: &[u16] = &[0, 1, 2, 3, 5, 4];
 
-        let pentagon_color = [to_srgb(0.5), 0.0, to_srgb(0.5)];
-        let pentagon_vertices: Vec<Vertex> = vec![
-            [-0.0868241, 0.49240386],
-            [-0.49513406, 0.06958647],
-            [-0.21918549, -0.44939706],
-            [0.35966998, -0.3473291],
-            [0.44147372, 0.2347359],
+        let pentagon_vertices: Vec<TexturedVertex> = vec![
+            TexturedVertex {
+                position: [-0.0868241, 0.49240386, 0.0],
+                tex_coords: [0.4131759, 0.99240386],
+            },
+            TexturedVertex {
+                position: [-0.49513406, 0.06958647, 0.0],
+                tex_coords: [0.0048659444, 0.56958647],
+            },
+            TexturedVertex {
+                position: [-0.21918549, -0.44939706, 0.0],
+                tex_coords: [0.28081453, 0.05060294],
+            },
+            TexturedVertex {
+                position: [0.35966998, -0.3473291, 0.0],
+                tex_coords: [0.85967, 0.1526709],
+            },
+            TexturedVertex {
+                position: [0.44147372, 0.2347359, 0.0],
+                tex_coords: [0.9414737, 0.7347359],
+            },
         ]
         .iter()
-        .map(|pos| Vertex {
-            position: [pos[0], pos[1], 0.0],
-            color: pentagon_color.clone(),
-        })
+        .map(
+            |TexturedVertex {
+                 position,
+                 tex_coords,
+             }| TexturedVertex {
+                position: *position,
+                tex_coords: [tex_coords[0], 1.0 - tex_coords[1]],
+            },
+        )
         .collect();
 
         let pentagon_indices: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
@@ -177,7 +225,7 @@ impl State {
 
         let swapchain_format = surface.get_preferred_format(&adapter).unwrap();
 
-        let mut config = wgpu::SurfaceConfiguration {
+        let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: swapchain_format,
             width: size.width,
@@ -187,32 +235,168 @@ impl State {
 
         surface.configure(&device, &config);
 
-        let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-            label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        let star_texture_bytes = include_bytes!("starofdavid2.png");
+        let star_texture =
+            texture::Texture::from_bytes(&device, &queue, star_texture_bytes, "starofdavid.png")
+                .unwrap();
+
+        let star_texture_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            multisampled: false,
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                ],
+                label: Some("star texture_bind_group_layout"),
+            });
+
+        let star_texture_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &star_texture_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&star_texture.view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&star_texture.sampler),
+                },
+            ],
+            label: Some("star diffuse_bind_group"),
         });
 
-        let render_pipeline_layout =
+        let tree_texture_bytes = include_bytes!("happy-tree.png");
+        let tree_texture =
+            texture::Texture::from_bytes(&device, &queue, tree_texture_bytes, "happy-tree.png")
+                .unwrap();
+
+        let tree_texture_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            multisampled: false,
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                ],
+                label: Some("tree texture_bind_group_layout"),
+            });
+
+        let tree_texture_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &tree_texture_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&tree_texture.view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&tree_texture.sampler),
+                },
+            ],
+            label: Some("tree diffuse_bind_group"),
+        });
+
+        let _color_shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+            label: Some("Color Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("color_shader.wgsl").into()),
+        });
+
+        let texture_shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+            label: Some("Texture Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("texture_shader.wgsl").into()),
+        });
+
+        let pentagon_render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
+                label: Some("Pentagon Render Pipeline Layout"),
+                bind_group_layouts: &[&tree_texture_bind_group_layout],
                 push_constant_ranges: &[],
             });
 
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
+        let pentagon_render_pipeline =
+            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Pentagon Render Pipeline"),
+                layout: Some(&pentagon_render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &texture_shader,
+                    entry_point: "vs_main",
+                    buffers: &[TexturedVertex::desc()],
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &texture_shader,
+                    entry_point: "fs_main",
+                    targets: &[wgpu::ColorTargetState {
+                        format: config.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    }],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    unclipped_depth: false,
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                multiview: None,
+            });
+
+        let star_render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Star Render Pipeline Layout"),
+                bind_group_layouts: &[&star_texture_bind_group_layout],
+                push_constant_ranges: &[],
+            });
+
+        let star_render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Star Render Pipeline"),
+            layout: Some(&star_render_pipeline_layout),
             vertex: wgpu::VertexState {
-                module: &shader,
+                // module: &_color_shader,
+                module: &texture_shader,
                 entry_point: "vs_main",
-                buffers: &[Vertex::desc()],
+                buffers: &[TexturedVertex::desc()],
             },
             fragment: Some(wgpu::FragmentState {
-                module: &shader,
+                // module: &_color_shader,
+                module: &texture_shader,
                 entry_point: "fs_main",
                 targets: &[wgpu::ColorTargetState {
                     format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
+                    blend: Some(wgpu::BlendState::ALPHA_BLENDING),
                     write_mask: wgpu::ColorWrites::ALL,
                 }],
             }),
@@ -221,11 +405,8 @@ impl State {
                 strip_index_format: None,
                 front_face: wgpu::FrontFace::Ccw,
                 cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
                 polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
                 unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
                 conservative: false,
             },
             depth_stencil: None,
@@ -240,7 +421,7 @@ impl State {
         let star_shape_buffers = ShapeBuffers {
             vertex_buffer: device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
                 label: Some("Star Vertex Buffer"),
-                contents: bytemuck::cast_slice(star_vertices.as_slice()),
+                contents: bytemuck::cast_slice(&star_vertices),
                 usage: wgpu::BufferUsages::VERTEX,
             }),
             index_buffer: device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
@@ -255,7 +436,7 @@ impl State {
         let pentagon_shape_buffers = ShapeBuffers {
             vertex_buffer: device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
                 label: Some("Pentagon Vertex Buffer"),
-                contents: bytemuck::cast_slice(pentagon_vertices.as_slice()),
+                contents: bytemuck::cast_slice(&pentagon_vertices),
                 usage: wgpu::BufferUsages::VERTEX,
             }),
             index_buffer: device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
@@ -273,11 +454,19 @@ impl State {
             queue,
             config,
             size,
-            render_pipeline,
-            star_shape_buffers,
-            pentagon_shape_buffers,
-            chosen_shape: ChosenShape::STAR,
+
             current_mouse_pos: None,
+            chosen_shape: ChosenShape::PENTAGON,
+
+            star_render_pipeline,
+            star_shape_buffers,
+            star_diffuse_texture_bind_group: star_texture_bind_group,
+            star_diffuse_texture: star_texture,
+
+            pentagon_render_pipeline,
+            pentagon_shape_buffers,
+            pentagon_diffuse_texture_bind_group: tree_texture_bind_group,
+            pentagon_diffuse_texture: tree_texture,
         }
     }
 
@@ -343,11 +532,22 @@ impl State {
                 depth_stencil_attachment: None,
             });
 
-            render_pass.set_pipeline(&self.render_pipeline);
-            let shape_buffers = match &self.chosen_shape {
-                ChosenShape::STAR => &self.star_shape_buffers,
-                ChosenShape::PENTAGON => &self.pentagon_shape_buffers,
+            let (shape_buffers, pipeline, bind_group) = match &self.chosen_shape {
+                ChosenShape::STAR => (
+                    &self.star_shape_buffers,
+                    &self.star_render_pipeline,
+                    Some(&self.star_diffuse_texture_bind_group),
+                ),
+                ChosenShape::PENTAGON => (
+                    &self.pentagon_shape_buffers,
+                    &self.pentagon_render_pipeline,
+                    Some(&self.pentagon_diffuse_texture_bind_group),
+                ),
             };
+            render_pass.set_pipeline(pipeline);
+            if let Some(bind_group) = bind_group {
+                render_pass.set_bind_group(0, bind_group, &[]);
+            }
             render_pass.set_vertex_buffer(0, shape_buffers.vertex_buffer.slice(..));
             render_pass.set_index_buffer(
                 shape_buffers.index_buffer.slice(..),
@@ -364,10 +564,6 @@ impl State {
 async fn run() {
     let event_loop = winit::event_loop::EventLoop::new();
     let window = winit::window::WindowBuilder::new()
-        // .with_inner_size(winit::dpi::LogicalSize::new(
-        //     FRAME_WIDTH as f64,
-        //     FRAME_HEIGHT as f64,
-        // ))
         .with_inner_size(winit::dpi::PhysicalSize::new(
             FRAME_WIDTH as f64,
             FRAME_HEIGHT as f64,
