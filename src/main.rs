ca2ffdcbdf6f19b2783e60d33cf57662f8f43e08
David Huculak <davidhuculak5@gmail.com>
2022-02-26 11:15:23 -0500

    remove commented out code

--- src/main.rs
+++ src/main.rs
@@ -145,28 +145,6 @@ impl State {
             color: pentagon_color.clone(),
         })
         .collect();
-        // let pentagon_vertices: &[Vertex] = &[
-        //     Vertex {
-        //         position: [-0.0868241, 0.49240386, 0.0],
-        //         color: pentagon_color.clone(),
-        //     },
-        //     Vertex {
-        //         position: [-0.49513406, 0.06958647, 0.0],
-        //         color: pentagon_color.clone(),
-        //     },
-        //     Vertex {
-        //         position: [-0.21918549, -0.44939706, 0.0],
-        //         color: pentagon_color.clone(),
-        //     },
-        //     Vertex {
-        //         position: [0.35966998, -0.3473291, 0.0],
-        //         color: pentagon_color.clone(),
-        //     },
-        //     Vertex {
-        //         position: [0.44147372, 0.2347359, 0.0],
-        //         color: pentagon_color.clone(),
-        //     },
-        // ];
 
         let pentagon_indices: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
@@ -301,38 +279,6 @@ impl State {
             chosen_shape: ChosenShape::STAR,
             current_mouse_pos: None,
         }
-
-        // wgpu docs say that wgsl is not supported on the web, but this seems to no longer
-        // be the case once v0.13.0 is released: https://github.com/gfx-rs/wgpu/pull/2315
-        // let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-        //     label: None,
-        //     source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
-        // });
-
-        // let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        //     label: None,
-        //     bind_group_layouts: &[],
-        //     push_constant_ranges: &[],
-        // });
-
-        // let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        //     label: None,
-        //     layout: Some(&pipeline_layout),
-        //     vertex: wgpu::VertexState {
-        //         module: &shader,
-        //         entry_point: "vs_main",
-        //         buffers: &[],
-        //     },
-        //     fragment: Some(wgpu::FragmentState {
-        //         module: &shader,
-        //         entry_point: "fs_main",
-        //         targets: &[swapchain_format.into()],
-        //     }),
-        //     primitive: wgpu::PrimitiveState::default(),
-        //     depth_stencil: None,
-        //     multisample: wgpu::MultisampleState::default(),
-        //     multiview: None,
-        // });
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
